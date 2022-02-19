53e42d7bc57d7cd7665e1b54d6d0b0faa83785fc
Alexey Rabkov <anrabkov@mail.ru>
2022-02-19 13:07:44 +0300

    Change something

---
+++ src/main/java/com/rabkov/musictracks/entity/NewEntity.java
@@ -0,0 +1,18 @@
+package com.rabkov.musictracks.entity;
+
+public class NewEntity{
+    private String name;
+    private int age;
+
+    public NewEntity(String name, int age, Role role) {
+        this.name = name;
+        this.age = age;
+        this.role = role;
+    }
+
+    private Role role;
+
+    public void fixRole(){
+        System.out.println(role.toString());
+    }
+}
